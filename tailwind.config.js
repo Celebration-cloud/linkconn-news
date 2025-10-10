df96f1a4a69b5b3ee550220e0c85ddf403e07ca6
Celebration-cloud <ojinguluc@gmail.com>
2025-10-10 08:06:25 +0100

    fixing styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-import { heroui } from "@heroui/react";
+import { heroui } from "@heroui/theme";
 
 /** @type {import('tailwindcss').Config} */
 const config = {
@@ -6,7 +6,7 @@ const config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/react/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
